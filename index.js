c0f157e82b5666b195a0e04834341db7c12114d5
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2019-12-04 17:39:13 -0500

    Update CSS & blog html

--- index.js
+++
@@ -1,8 +0,0 @@
-function navFunction() {
-  var x = document.getElementById("hamburger-nav");
-  if (x.className === "burger-nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "burger-nav";
-  }
-}
